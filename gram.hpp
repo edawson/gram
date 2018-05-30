d39ec08ca91dc523e308dc0c657706f4fce16f60
Eric T. Dawson <eric.t.dawson@gmail.com>
2018-05-30 12:08:09 +0100

    Fill in gram data structures

--- gram.hpp
+++ gram.hpp
@@ -2,6 +2,7 @@
 #define GRAM_HPP_DEF
 #include <stdio>
 #include <vector>
+#include <bitset>
 #include <fstream>
 #include <string>
 
@@ -11,29 +12,63 @@ struct gram_tag_elem{
 
 
 struct gram_header_elem{
-
+    char* id;
+    char* type;
+    void* val;
 };
 
-class GramRecord{
+struct GramRecord{
     
+    char* contig;
+    uint32_t start;
+    uint32_t end;
+    char* identifier;
+    char* sequence;
+    uint32_t length;
+    char* qualities;
+    std::vector<char*> path;
+    std::vector<char*> cigars;
+    std::vector<gram_tag_elem> tags;
+    std::bitset<32> flags;
+    bool isMapped(){
+        
+    };
+    void isMapped(bool m){
+
+    };
+    bool isSoftclipped(){
+        
+    };
+    void isMapped(bool m){
+
+    }
+    
+    GramRecord();
+    GramRecord(string line);
 
     bool compare(const GramRecord& other);
     void write_to_stream(ostream& os);
+    string collapsed_cigar();
     string to_string();
     string to_SAM(string ref_path);
+    string to_BED();
 };
 
-class GramCollection{
-    public:
-        std::string get_header();
-        void parse_gram(const char* gram_file);
-        void parse_gram(string gram_file);
-        void parse_gram(ifstream gram_file);
 
-    private:
-        vector<gram_header_elem> header;
+inline void parseLine(char* line, GramRecord& g){
 
 };
 
+inline void parseLine(string line, GramRecord& g){
+
+};
+
+inline void parseFromGAM(char* line, GramRecord& g){
+
+};
+
+inline void parseFromGAM(string line, GramRecord& g){
+
+};
 
 #endif
